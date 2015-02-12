3b95e6f30b77b1b3665e96a2c619031d84aa939f
avi <avinashsajjan@gmail.com>
2015-02-12 17:24:13 +0530

    added custom table cell view and corresponding controller. Also connected custom table controller to storyboard

---
+++ FoodPin/CustomTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  CustomTableViewCell.swift
+//  FoodPin
+//
+//  Created by avi on 12/02/15.
+//  Copyright (c) 2015 avi. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var restaurantNameLabel: UILabel!
+    @IBOutlet weak var restaurantLocationLabel: UILabel!
+    @IBOutlet weak var restaurantTypeLabel: UILabel!
+    @IBOutlet weak var restaurantThumbnail: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
