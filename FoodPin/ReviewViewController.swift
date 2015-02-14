3fcbdd86b0c02a314472523bcc8383eed2b9e171
avi <avinashsajjan@gmail.com>
2015-02-14 23:10:31 +0530

    added ReviewViewController for Review View. And also added blur effect to the background,

---
+++ FoodPin/ReviewViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ReviewViewController.swift
+//  FoodPin
+//
+//  Created by avi on 14/02/15.
+//  Copyright (c) 2015 avi. All rights reserved.
+//
+
+import UIKit
+
+class ReviewViewController: UIViewController {
+
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        var blurEffect = UIBlurEffect(style: .Dark)
+        var blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = view.bounds
+        backgroundImageView.addSubview(blurEffectView)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
