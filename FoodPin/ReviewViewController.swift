85439d0b76dbeec6181a950b22149d8717a22842
avi <avinashsajjan@gmail.com>
2015-02-14 23:32:28 +0530

    added simple animation to review dialog.

--- FoodPin/ReviewViewController.swift
+++ FoodPin/ReviewViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ReviewViewController: UIViewController {
 
     @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var dialogView: UIView!
     
     var backgroundImage: String!
     
@@ -18,11 +19,17 @@ class ReviewViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        // sets the background to the restarunat's image. this data has been 
+        // sent by detail view, in prepareSegue
         backgroundImageView.image = UIImage(named: backgroundImage)
+        // following lines add blur effect
         var blurEffect = UIBlurEffect(style: .Dark)
         var blurEffectView = UIVisualEffectView(effect: blurEffect)
         blurEffectView.frame = view.bounds
         backgroundImageView.addSubview(blurEffectView)
+        
+        // scales down the view when loaded. and then we use grow effect
+        dialogView.transform = CGAffineTransformMakeScale(0.0, 0.0)
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,6 +37,12 @@ class ReviewViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewDidAppear(animated: Bool) {
+        // following lines add grow effect to the scaled down version of dialogview
+        UIView.animateWithDuration(0.7, delay: 0.0, options: nil, animations: {
+                self.dialogView.transform = CGAffineTransformMakeScale(1.0, 1.0)
+            }, completion: nil)
+    }
 
     /*
     // MARK: - Navigation
