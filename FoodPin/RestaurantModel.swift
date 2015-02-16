67a8a4eee06e8b377000dffebd44e6b48eade815
avi <avinashsajjan@gmail.com>
2015-02-16 13:07:28 +0530

    added Data Model which corresponds to Restaurant class. And also modified Restaurant Class, so that now its a managed object model

--- FoodPin/RestaurantModel.swift
+++ FoodPin/RestaurantModel.swift
@@ -7,17 +7,12 @@
 //
 
 import Foundation
+import CoreData
 
-class Restaurant {
-    var name, type, location, image, phoneNum: String
-    var isVisited: Bool
-    
-    init(name: String, type: String, location: String, image: String, isVisited: Bool, phoneNum: String) {
-        self.name = name
-        self.type = type
-        self.location = location
-        self.image = image
-        self.isVisited = isVisited
-        self.phoneNum = phoneNum
-    }
+class Restaurant: NSManagedObject {
+    @NSManaged var name: String!
+    @NSManaged var type: String!
+    @NSManaged var location: String!
+    @NSManaged var image: NSData!
+    @NSManaged var isVisited: NSNumber!
 }
